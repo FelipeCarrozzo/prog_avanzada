9342328bdefa42d9120b891ac82b07d371761f91
FelipeCarrozzo <felipe.carrozzo@ingenieria.uner.edu.ar>
2025-03-27 16:47:09 -0300

    añado en guardar_usuario parametros 'aciertos' y 'fechayhora'

--- TrabajoPractico_1/proyecto_1/modules/info_usuario.py
+++ TrabajoPractico_1/proyecto_1/modules/info_usuario.py
@@ -8,21 +8,21 @@ def agregar_usuario_nfrases(lista_usuarios, n_usuario, n_frases):
         }
     lista_usuarios.append(datos_usuario) 
 
-def cargar_lista(nombre_archivo, lista_libros):
+def cargar_lista(nombre_archivo, lista_usuarios):
     """Función que lee la información de los usuarios desde un archivo
     y lo carga a una lista.
     """
     with open(nombre_archivo, "r") as archi:
         for linea in archi:
             libro = linea.rstrip().split(',')
-            agregar_usuario_nfrases(lista_libros, libro[0], libro[1]) 
+            agregar_usuario_nfrases(lista_usuarios, libro[0], libro[1]) 
 
-def guardar_usuario_en_archivo(nombre_archivo, n_usuario: str, n_frases: int): 
-    """Guarda la información de un usuario y cant frases en archivo
+def guardar_usuario_en_archivo(nombre_archivo, n_usuario: str, n_frases: int, aciertos: int, fechayhora: str): 
+    """Guarda la información del usuario: 
+    nombre de usuario, num de frases, aciertos, fecha y hora del inicio de la trivia.
     """   
     with open(nombre_archivo, "a") as archi:
-        archi.write(f"{n_usuario},{n_frases}\n")
+        archi.write(f"{n_usuario},{n_frases},{aciertos},{fechayhora}\n")
 
 if __name__ == '__main__':
-    lista = []
-    a = agregar_usuario_nfrases(lista, "Tapalin", 3)
+    guardar_usuario_en_archivo("./data/datos_usuario.txt", "usuario1", 5, 3, "2021-09-01 12:00:00")
