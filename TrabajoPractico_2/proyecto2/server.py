2c59a3d63f6ebb8820655f3f2eae8c9b08c27093
FelipeCarrozzo <felipe.carrozzo@ingenieria.uner.edu.ar>
2025-04-29 15:02:40 -0300

    agrego un diccionario con clave del calculo y el valor calculado para cada Clase

--- TrabajoPractico_2/proyecto2/server.py
+++ TrabajoPractico_2/proyecto2/server.py
@@ -4,24 +4,23 @@ from modules.config import app
 from modules.cinta import Cinta
 from modules.detector import DetectorAlimento
 from modules.cajon import Cajon
+from modules.calculador import CalculadorBromatologico
+from modules.alimentos import Alimentos, Verdura, Fruta, Kiwi, Manzana, Papa, Zanahoria
 
-app.secret_key = "trabajo2_PA"
 
 # Página de inicio
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    alimentos = []
+    alimentos = [] #borrar antes de entregar
+    detector = DetectorAlimento()
+    cajon = Cajon()
+    calculador = CalculadorBromatologico()
+    cinta = Cinta(detector, cajon)
     if request.method == 'POST':
-        
         #genero objetos
         n_alimentos = int(request.form.get("n_alimentos"))  #viene desde html
         if n_alimentos:
             print("se detectó el número de alimentos")
-            # session['n_alimentos'] = int(n_alimentos)  #convierte a entero
-            detector = DetectorAlimento()
-            cajon = Cajon(n_alimentos)
-            cinta = Cinta(detector, cajon)
-
             contador = 0
             while contador < n_alimentos:
                 print("Alimento en la cinta: ")
@@ -32,8 +31,19 @@ def index():
                     contador += 1
                 else:
                     print("Alimento inválido")
-            
-        print("Alimentos en la sesión:", cajon.mostrar_contenido_cajon())
+
+    calculos_aw_promedio = {
+    "aw_kiwi": calculador.calcular_aw(Kiwi,cajon),
+    "aw_manzana": calculador.calcular_aw(Manzana,cajon),
+    "aw_papa": calculador.calcular_aw(Papa,cajon),
+    "aw_zanahoria": calculador.calcular_aw(Zanahoria,cajon),
+    "aw_frutas": calculador.calcular_aw(Fruta,cajon),
+    "aw_verduras": calculador.calcular_aw(Verdura,cajon),
+    "aw_total": calculador.calcular_aw(Alimentos,cajon)
+    }
+
+    print("Valores de aw promedio:", calculos_aw_promedio)
+
     
 
     return render_template('index.html', lista_alimentos=alimentos, n_alimentos=session.get("n_alimentos"))
