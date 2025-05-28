b1a5ae332cf35776ef0d2267c4405a8cbcad6dfc
FelipeCarrozzo <felipe.carrozzo@ingenieria.uner.edu.ar>
2025-05-28 14:34:20 -0300

    Co-authored-by: Agustina Caporizzo agustina.caporizzo@ingenieria.uner.edu.ar trabajamos sobre el registro del usuario. Generamos formularios de Flask, pagina de registro.

--- TrabajoPractico_3/modules/config.py
+++ TrabajoPractico_3/modules/config.py
@@ -1,3 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 app = Flask("server")
+SECRET_KEY = "d87h3dxodj09j30"
+app.config['SECRET_KEY'] = SECRET_KEY
--- TrabajoPractico_3/server.py
+++ TrabajoPractico_3/server.py
@@ -1,11 +1,42 @@
-# Ejemplo de aplicación principal en Flask
-from flask import render_template
-from modules.config import app
+from flask_wtf import FlaskForm
+from flask import Flask, request, render_template, flash, redirect, url_for, session
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
+from modules.config import SECRET_KEY
+# from modules.gestorDeUsuarios import GestorUsuarios
+from modules.formularios import RegistroForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+# app.config.from_object('config') 
 
 # Página de inicio
-@app.route('/')
-def index():
-    return render_template('inicio.html')
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#         form = LoginForm()
+#         if form.validate_on_submit():
+#             # Procesamiento del login
+#             username = form.username.data
+#             password = form.password.data
+#             print(f'Usuario: {username}, Contraseña: {password}')
+#             return redirect(url_for('dashboard'))
+
+#     return render_template('login.html', form=form)
+
+#pagina de registro
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    form = RegistroForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        # Aquí podrías guardar el usuario en una base de datos
+        print(f"Nuevo usuario: {username}, Email: {email}, Contraseña: {password}")
+        flash('Usuario registrado exitosamente', 'success')
+        # return redirect(url_for('login'))  # Redirigís a la página de login u otra
+
+    return render_template('registro.html', form=form)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
